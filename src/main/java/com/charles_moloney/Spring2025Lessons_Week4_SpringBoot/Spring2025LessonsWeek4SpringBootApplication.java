f6938204f9c0797f905b76c69585399ab260ea6b
10625639 <cmoloney2@huskers.unl.edu>
2025-05-09 13:13:08 -0500

    Initial commit

---
+++ src/main/java/com/charles_moloney/Spring2025Lessons_Week4_SpringBoot/Spring2025LessonsWeek4SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.charles_moloney.Spring2025Lessons_Week4_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2025LessonsWeek4SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2025LessonsWeek4SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charles_moloney/Spring2025Lessons_Week4_SpringBoot/Spring2025LessonsWeek4SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles_moloney.Spring2025Lessons_Week4_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2025LessonsWeek4SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
